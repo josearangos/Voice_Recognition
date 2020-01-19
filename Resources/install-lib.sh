9896b05f93c990ef3c1256bb3b5f09d7f64b9a94
deiry <sofia.navas96@gmail.com>
2020-01-19 15:42:07 -0500

    resources

--- Resources/install-lib.sh
+++ Resources/install-lib.sh
@@ -1,5 +1,16 @@
+<<<<<<< HEAD
 python -m pip install -U pip
 python -m pip install -U matplotlib
 pip install numpy
 pip install pandas
 pip install -U scikit-learn
+=======
+pip3 install numpy
+pip3 install pixiedust
+pip3 install -U matplotlib
+pip3 install -U scikit-learn
+pip3 install pandas
+pip3 install qgrid
+jupyter nbextension enable --py --sys-prefix qgrid
+pip3 install mlxtend  
+>>>>>>> 1530fb3d2e8d900b639ca1147abe9496110df989
